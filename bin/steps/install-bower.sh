a2b21de5ccdf36f1bef117adcf0070658123461b
Tom Van Damme <tom@rotorgroup.be>
2015-02-18 18:11:39 +0100

    Fix bug

--- bin/steps/install-bower.sh
+++ bin/steps/install-bower.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+current_dir_cache=$PWD
+
 # Install bower dependencies
 if [ -e $build_dir/bower.json ]; then
   status "Install bower dependencies"
   if test -d $build_dependencies_cache/bower; then
     status "Restoring bower components from cache"
     cp -r $build_dependencies_cache/bower $build_dir/bower_components
+
+    cd $build_dir
     HOME=$build_dir $build_dir/node_modules/.bin/bower install 2>&1 | indent
+    cd $current_dir_cache
+
   else
+
+    cd $build_dir
     HOME=$build_dir $build_dir/node_modules/.bin/bower install  2>&1 | indent
+    cd $current_dir_cache
+
   fi
 
   # Cache bower
