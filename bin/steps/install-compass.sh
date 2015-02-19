6447bf411b42e0d41d9ba409d7a378f334fdb93b
Tom Van Damme <tom@rotorgroup.be>
2015-02-19 01:19:22 +0100

    test

--- bin/steps/install-compass.sh
+++ bin/steps/install-compass.sh
@@ -3,14 +3,14 @@
 # Install compass
 status "Installing Compass"
 RUBY_VERSION=$(ruby -e "require 'rbconfig';puts \"#{RUBY_ENGINE}/#{RbConfig::CONFIG['ruby_version']}\"")
-export GEM_HOME=$build_dependencies/.gem/$RUBY_VERSION
+export GEM_HOME=$build_dir/.gem/$RUBY_VERSION
 PATH="$GEM_HOME/bin:$PATH"
 if test -d $build_dependencies_cache/ruby/.gem; then
   status "Restoring ruby gems directory from cache"
-  cp -r $build_dependencies_cache/ruby/.gem $build_dependencies
-  HOME=$build_dependencies gem update compass --user-install --no-rdoc --no-ri 2>&1 | indent
+  cp -r $build_dependencies_cache/ruby/.gem $build_dir
+  HOME=$build_dir gem update compass --user-install --no-rdoc --no-ri 2>&1 | indent
 else
-  HOME=$build_dependencies gem install compass --user-install --no-rdoc --no-ri 2>&1 | indent
+  HOME=$build_dir gem install compass --user-install --no-rdoc --no-ri 2>&1 | indent
 fi
 
 # Cache ruby gems
@@ -18,7 +18,7 @@ rm -rf $build_dependencies_cache/ruby
 mkdir -p $build_dependencies_cache/ruby
 
 # If app has a gems directory, cache it.
-if test -d $build_dependencies/.gem; then
+if test -d $build_dir/.gem; then
   status "Caching ruby gems directory for future builds"
-  cp -r $build_dependencies/.gem $build_dependencies_cache/ruby
+  cp -r $build_dir/.gem $build_dependencies_cache/ruby
 fi
